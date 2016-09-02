6d8790c2ac108a0328d86e51c346ea4a4060b5cd
erinversfeld <erinversfeld@gmail.com>
2016-09-02 20:47:45 +0200

    [file structure] reorgnised file structure, renamed directories

---
+++ ply/test/pkg_test3/__init__.py
@@ -0,0 +1,7 @@
+# Tests proper handling of lextab and parsetab files in package structures
+
+# Here for testing purposes
+import sys
+if '..' not in sys.path:  
+    sys.path.insert(0, '..')
+
---
+++ ply/test/pkg_test6/__init__.py
@@ -0,0 +1,7 @@
+# Tests proper sorting of modules in yacc.ParserReflect.get_pfunctions
+
+# Here for testing purposes
+import sys
+if '..' not in sys.path:
+    sys.path.insert(0, '..')
+
